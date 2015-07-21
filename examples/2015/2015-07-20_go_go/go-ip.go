88dee295790a56e4aba5354a5f251ff6fd700c1a
julien <darul75@gmail.com>
2015-07-22 00:01:28 +0200

    |

--- examples/2015/2015-07-20_go_go/go-ip.go
+++ examples/2015/2015-07-20_go_go/go-ip.go
@@ -1,103 +1,44 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "os"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
 )
 
-// http://stackoverflow.com/questions/2270670/trouble-reading-from-a-socket-in-go
+//http://stackoverflow.com/questions/21270945/how-to-read-the-response-from-a-newsinglehostreverseproxy
 
-func handle(conn *net.IPConn) {
-    fmt.Printf("Connection from %s\n", conn.RemoteAddr());
-    message := make([]byte, 1024);
-    // TODO: loop the read, we can have >1024 bytes
-    n1, error := conn.Read(message);
-    if error != nil {
-        fmt.Printf("Cannot read: %s\n", error);
-        os.Exit(1);
-    }
+func NewHttpClient() *http.Client {
+	return &http.Client{}
+}
 
-    fmt.Printf("Echoed %d bytes\n", message[0:n1]);
-    n2, error := conn.Write(message[0:n1]);
-    if error != nil || n2 != n1 {
-        fmt.Printf("Cannot write: %s\n", error);
-        os.Exit(1);
-    }
-    fmt.Printf("Echoed %d bytes\n", n2);
+func handler(w http.ResponseWriter, r *http.Request) {
+	client := NewHttpClient()
+	//r.Host = "127.0.0.1:8081"
+	// func NewRequest(method, urlStr string, body io.Reader) (*Request, error)
+	fmt.Println("seeking")
+	target := "http://127.0.0.1:8081" + r.URL.Path
+	fmt.Println(target)
+	r.Host = "http://127.0.0.1:8081"
+	newReq, _ := http.NewRequest(r.Method, "http://127.0.0.1:8081"+r.URL.Path, r.Body)
+	response, err := client.Do(newReq)
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+	}
+	fmt.Println(response)
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	tmpl, err := template.New("name").Parse(string(body))
 
-    conn.Close();   // TODO: wait to see if more data? It would be better with telnet...
+	fmt.Fprintf(w, "%s", tmpl)
+
+	//fmt.Fprintf(w, "%s", body)
 }
 
 func main() {
-    /*protocol := "icmp"
-    netaddr, _ := net.ResolveIPAddr("ip4", "127.0.0.1")
-    conn, _ := net.ListenIP("ip4:"+protocol, netaddr)*/
+	http.HandleFunc("/", handler)
 
-	//conn, err:= net.Dial("tcp", "google.com:http")
-	/*conn, err:= net.Dial("ip4:1", "127.0.0.1")
-
-	checkConnection(conn, err)*/
-
-	// func Dial(network, address string) (Conn, error)
-	/*conn, err :=net.Dial("udp", "tst3.creditagricole.info:80")
-	checkConnection(conn, err)
-
-	buf := make([]byte, 1024)
-    numRead, _:= conn.Read(buf)
-    fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
-    fmt.Printf("% X\n", buf[:numRead])*/
-
-    // func ListenPacket(net, laddr string) (PacketConn, error)
-    /*protocol := "ip"
-    netaddr, _ := net.ResolveIPAddr("ip4", "127.0.0.1")
-    conn, _ := net.ListenIP("ip4:"+protocol, netaddr)
-
-    buf := make([]byte, 1024)
-    numRead, _, _ := conn.ReadFrom(buf)
-    fmt.Printf("% X\n", buf[:numRead])*/
-
-   /* l, err := net.ListenPacket("udp", "127.0.0.1:0")
-    	if err != nil {
-    		fmt.Printf("Listen failed: %v", err)
-    }
-
-    fmt.Printf(l.LocalAddr().String())
-
-    buf2 := make([]byte, 1024)
-    for {
-    	numRead2, _, _ := l.ReadFrom(buf2)
-		fmt.Printf("% X\n", buf2[:numRead2])
-    }*/
-
-
-	/*conn, err =net.Dial("tcp", "[2620:0:2d0:200::10]:80")
-	checkConnection(conn, err)*/
-
-	listen := ":7";
-    addr, error := net.ResolveIPAddr("ip4", "127.0.0.1");
-    if error != nil {
-        fmt.Printf("Cannot parse \"%s\": %s\n", listen, error);
-        os.Exit(1);
-    }
-    conn, error := net.ListenIP("ip", addr);
-    if error != nil {
-        fmt.Printf("Cannot listen: %s\n", error);
-        os.Exit(1);
-    }
-    for {   // ever...
-
-        go handle(conn);
-    }
-
-}
-
-func checkConnection(conn net.Conn, err error) {
-	if err!= nil {
-		fmt.Printf("error %v connecting!", err)
-		os.Exit(1)
-	}
-
-	fmt.Println("Connection is made with %v", conn)
-
+	http.ListenAndServe(":8080", nil)
 }
--- examples/2015/2015-07-20_go_go/go.go
+++ examples/2015/2015-07-20_go_go/go.go
@@ -2,120 +2,65 @@ package main
 
 import (
 	"bufio"
-    "fmt"
-    "io"
-    "net"
-    "os"
+	"fmt"
+	"io"
+	"net"
+	"os"
 )
 
 // http://stackoverflow.com/questions/2270670/trouble-reading-from-a-socket-in-go
 
 func handle(conn *net.TCPConn) {
-    fmt.Printf("Connection from %s\n", conn.RemoteAddr());
-    message := make([]byte, 1024);
-    // TODO: loop the read, we can have >1024 bytes
-    // n1, error := conn.Read(message);
+	fmt.Printf("Connection from %s\n", conn.RemoteAddr())
+	// TODO: loop the read, we can have >1024 bytes
+	// n1, error := conn.Read(message);
 
-    buf := bufio.NewReader(conn);
+	buf := bufio.NewReader(conn)
 
-    for {
-    	n1, err := buf.Read(message);
-    	n2, error := conn.Write(message[0:n1]);
-	    if error != nil || n2 != n1 {
-	        fmt.Printf("Cannot write: %s\n", error);
-	        os.Exit(1);
-	    }
+	for {
+		message := make([]byte, 1024)
+		n1, err := buf.Read(message)
+		//n2, error := conn.Write(message[0:n1])
+		fmt.Printf("Echoed %s string\n", message[0:n1])
+		// fmt.Printf("Echoed %d bytes\n", message[0:n1])
+		// if error != nil || n2 != n1 {
+		// 	fmt.Printf("Cannot write: %s\n", error)
+		// 	os.Exit(1)
+		// }
 
-    	if err == io.EOF {
-    		//
-    	}
-    }
+		if err == io.EOF {
+			break
+		}
+	}
 
-    /*if error != nil {
-        fmt.Printf("Cannot read: %s\n", error);
-        os.Exit(1);
-    }*/
-
-    //fmt.Printf("Echoed %d bytes\n", message[0:n1]);
-    /*n2, error := conn.Write(message[0:n1]);
-    if error != nil || n2 != n1 {
-        fmt.Printf("Cannot write: %s\n", error);
-        os.Exit(1);
-    }
-    fmt.Printf("Echoed %d bytes\n", n2);*/
-
-    conn.Close();   // TODO: wait to see if more data? It would be better with telnet...
+	conn.Close()
 }
 
 func main() {
-    /*protocol := "icmp"
-    netaddr, _ := net.ResolveIPAddr("ip4", "127.0.0.1")
-    conn, _ := net.ListenIP("ip4:"+protocol, netaddr)*/
-
-	//conn, err:= net.Dial("tcp", "google.com:http")
-	/*conn, err:= net.Dial("ip4:1", "127.0.0.1")
-
-	checkConnection(conn, err)*/
-
-	// func Dial(network, address string) (Conn, error)
-	/*conn, err :=net.Dial("udp", "tst3.creditagricole.info:80")
-	checkConnection(conn, err)
-
-	buf := make([]byte, 1024)
-    numRead, _:= conn.Read(buf)
-    fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
-    fmt.Printf("% X\n", buf[:numRead])*/
-
-    // func ListenPacket(net, laddr string) (PacketConn, error)
-    /*protocol := "ip"
-    netaddr, _ := net.ResolveIPAddr("ip4", "127.0.0.1")
-    conn, _ := net.ListenIP("ip4:"+protocol, netaddr)
-
-    buf := make([]byte, 1024)
-    numRead, _, _ := conn.ReadFrom(buf)
-    fmt.Printf("% X\n", buf[:numRead])*/
-
-   /* l, err := net.ListenPacket("udp", "127.0.0.1:0")
-    	if err != nil {
-    		fmt.Printf("Listen failed: %v", err)
-    }
-
-    fmt.Printf(l.LocalAddr().String())
-
-    buf2 := make([]byte, 1024)
-    for {
-    	numRead2, _, _ := l.ReadFrom(buf2)
-		fmt.Printf("% X\n", buf2[:numRead2])
-    }*/
-
-
-	/*conn, err =net.Dial("tcp", "[2620:0:2d0:200::10]:80")
-	checkConnection(conn, err)*/
-
-	listen := ":7";
-    addr, error := net.ResolveTCPAddr("tcp", "127.0.0.1:8082");
-    if error != nil {
-        fmt.Printf("Cannot parse \"%s\": %s\n", listen, error);
-        os.Exit(1);
-    }
-    listener, error := net.ListenTCP("tcp", addr);
-    if error != nil {
-        fmt.Printf("Cannot listen: %s\n", error);
-        os.Exit(1);
-    }
-    for {   // ever...
-        conn, error := listener.AcceptTCP();
-        if error != nil {
-            fmt.Printf("Cannot accept: %s\n", error);
-            os.Exit(1);
-        }
-        go handle(conn);
-    }
+	listen := ":7"
+	addr, error := net.ResolveTCPAddr("tcp", "0.0.0.0:8081")
+	if error != nil {
+		fmt.Printf("Cannot parse \"%s\": %s\n", listen, error)
+		os.Exit(1)
+	}
+	listener, error := net.ListenTCP("tcp", addr)
+	if error != nil {
+		fmt.Printf("Cannot listen: %s\n", error)
+		os.Exit(1)
+	}
+	for { // ever...
+		conn, error := listener.AcceptTCP()
+		if error != nil {
+			fmt.Printf("Cannot accept: %s\n", error)
+			os.Exit(1)
+		}
+		go handle(conn)
+	}
 
 }
 
 func checkConnection(conn net.Conn, err error) {
-	if err!= nil {
+	if err != nil {
 		fmt.Printf("error %v connecting!", err)
 		os.Exit(1)
 	}
