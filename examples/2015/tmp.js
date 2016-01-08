2210669c3632357c9a00b0ef3f282e396e5f73d7
Julien Valéry <darul75@gmail.com>
2016-01-08 18:44:45 +0100

    |

---
+++ examples/2015/tmp.js
@@ -0,0 +1,42 @@
+var init = {
+	method: 'GET',
+	mode: 'cors'
+};
+
+myRequest = new Request('https://www.google.fr/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', init);
+
+fetch(myRequest).then(function(response) {
+  return response.blob();
+}).then(function(response) {
+  var blob = response;
+  console.log(blob);
+
+  var reader = new FileReader();
+
+	reader.addEventListener("loadend", function() {
+	   // reader.result contains the contents of blob as a typed array
+	   console.log('end');
+	   var buffer = reader.result;
+
+	   // read first eight bytes
+	   var data = new Uint8Array(buffer);
+	   /*console.log(header);
+	   console.log(buffer);
+	   console.log(buffer.length);*/
+
+	   var image = document.createElement('img');
+	   image.style.position = "absolute";
+	   image.style.right = "10px";
+	   image.style.top = "10px";
+
+	   var base64Data = btoa(String.fromCharCode.apply(null, data));
+       image.src = 'data:image/png;base64,' + base64Data;
+       document.getElementsByTagName('body')[0].appendChild(image);
+	});
+
+  //reader.readAsText(blob);
+  //console.log(text);
+  reader.readAsArrayBuffer(blob);
+});
+
+// CORS ISSUE
